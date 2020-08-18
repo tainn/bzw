41df1124804aa559f207f5e9ad757c55a567f2d7
Peter Levart <tainn@protonmail.com>
2020-08-18 15:09:46 +0200

    Adjusts docstrings and updates readme

--- README.md
+++ README.md
@@ -1,15 +1,14 @@
 # bzwrap
-![Version](https://img.shields.io/badge/version-v1.0-blue)
-![License](https://img.shields.io/badge/license-GPLv3-orange)
-
-`bzwrap` (or **bzw wrapper**) is a package written in Python that allows for simple bzw object creation and file population.
+`bzwrap` (or `bzw-wrapper`) is a package written in Python that allows for simple bzw object creation and file population.
 
 It features no hard restrictions, meaning that any object can be created with any attribute, even if the object itself does not exist. As such this package is merely an extension to creating maps by hand, making it more compact, better organized, and allowing the inclusion of logic while building bzw objects.
 
 ## Install
-Fetch the latest version of the package:
+Fetch the latest version of the package
 
-`pip3 install --user --upgrade git+git://github.com/tainn/bzwrap.git`
+```sh
+pip3 install --user --upgrade git+git://github.com/tainn/bzwrap.git
+```
 
 ## Usage
 ```py
@@ -38,7 +37,7 @@ bzw.comment('This is a comment')
 bzw.comment('This comment has two new lines afterwards', addline=True)
 ```
 
-## Logic
+### Logic
 By utilizing some form of logic, one line doesn't have to equal just one object creation.
 
 ```py
@@ -51,5 +50,5 @@ for i in range(-4, 5):
     bzw.create('meshbox', position=(i * 40, i * 40, 0), rotation=i * 10, size=(10, 10, 10 * abs(i) + 10))
 ```
 
-## Output
-The output of running such a script is a `my_map.bzw` file, created in the same directory as the executable, which includes the objects created during runtime.
+### Output
+The output of running such a script is a `my_map.bzw` file, created in the same directory as the executable, which features the objects created during runtime.
--- bzwrap/bzwrap.py
+++ bzwrap/bzwrap.py
@@ -2,16 +2,20 @@
 
 
 class Bzw:
-    """Allows for creation of bzw objects and writing them to a file"""
+    """
+    Allows for creation of bzw objects and writing them to a file
+    """
 
     def __init__(self, filename: str) -> None:
-        """Sets the name of the bzw file"""
-
+        """
+        Sets the name of the bzw file
+        """
         self.filename: str = filename if filename.endswith('.bzw') else f'{filename}.bzw'
 
     def create(self, ref: str, group: bool = False, **kwargs) -> None:
-        """Creates and appends the specified object to a bzw file"""
-
+        """
+        Creates and appends the specified object to a bzw file
+        """
         with open(self.filename, 'a') as f:
             f.write(f'{ref}\n' if not group else f'group {ref}\n')
 
@@ -25,25 +29,29 @@ class Bzw:
             f.write('end\n\n')
 
     def define(self, name: str = None, end: bool = False) -> None:
-        """Creates an opening or closing line of a group definition"""
-
+        """
+        Creates an opening or closing line of a group definition
+        """
         with open(self.filename, 'a') as f:
             f.write(f'define {name}\n\n' if not end else 'enddef\n\n')
 
     def include(self, path: str) -> None:
-        """Creates an include line"""
-
+        """
+        Creates an include line
+        """
         with open(self.filename, 'a') as f:
             f.write(f'include {path}\n\n')
 
     def emptyline(self, amount: int = 1) -> None:
-        """Creates the specified amount of empty lines"""
-
+        """
+        Creates the specified amount of empty lines
+        """
         with open(self.filename, 'a') as f:
             f.write('\n' * amount)
 
     def comment(self, content: str, addline: bool = False) -> None:
-        """Creates a comment with the passed content"""
-
+        """
+        Creates a comment with the passed content
+        """
         with open(self.filename, 'a') as f:
             f.write(f'# {content}\n' if not addline else f'# {content}\n\n')
