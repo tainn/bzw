b681d0813b5c04ac9332b7a1b69e7bb81f36b12e
tainn <tainn@protonmail.com>
2022-08-05 18:52:02 +0200

    Version bump to 2.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="bzw-wrapper",
-    version="2.1",
+    version="2.2",
     packages=["bzw"],
     url="https://github.com/tainn/bzw-wrapper",
     license="MIT",
