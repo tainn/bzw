f748a20ee5f9ada303f921fd17df3a683e1bbafa
tainn <tainn@protonmail.com>
2022-10-06 00:17:36 +0200

    Prefix the variable with append info

--- bzw/bzw.py
+++ bzw/bzw.py
@@ -6,48 +6,48 @@ class Bzw:
         self.filename: str = utils.set_filename(filename=filename, overwrite=overwrite)
 
     def create(self, typ: str, group: bool = False, **kwargs) -> None:
-        with open(self.filename, "a") as f:
+        with open(self.filename, "a") as af:
             if not group:
-                f.write(f"{typ}\n")
+                af.write(f"{typ}\n")
             else:
-                f.write(f"group {typ}\n")
+                af.write(f"group {typ}\n")
 
             for attr in kwargs:
                 # Non-array (fixed depth 0)
                 if isinstance(kwargs[attr], (str, int, float)):
-                    f.write(f"{attr.strip('_')} {kwargs[attr]}\n")
+                    af.write(f"{attr.strip('_')} {kwargs[attr]}\n")
 
                 # Array of non-arrays (fixed depth 1)
                 elif not any(isinstance(entry, (list, tuple, dict)) for entry in kwargs[attr]):
                     kwargs[attr]: str = utils.deep_type_cast(core=kwargs, part=attr, name=attr)
-                    f.write(f"{attr.strip('_')} {kwargs[attr]}\n")
+                    af.write(f"{attr.strip('_')} {kwargs[attr]}\n")
 
                 # Array of arrays (max depth 2)
                 else:
                     for idx, _ in enumerate(kwargs[attr]):
                         partial: str = utils.deep_type_cast(core=kwargs[attr], part=idx, name=attr)
-                        f.write(f"{attr.strip('_')} {partial}\n")
+                        af.write(f"{attr.strip('_')} {partial}\n")
 
-            f.write("end\n\n")
+            af.write("end\n\n")
 
     def define(self, name: str = None, end: bool = False) -> None:
-        with open(self.filename, "a") as f:
+        with open(self.filename, "a") as af:
             if not end:
-                f.write(f"define {name}\n\n")
+                af.write(f"define {name}\n\n")
             else:
-                f.write("enddef\n\n")
+                af.write("enddef\n\n")
 
     def include(self, path: str) -> None:
-        with open(self.filename, "a") as f:
-            f.write(f"include {path}\n\n")
+        with open(self.filename, "a") as af:
+            af.write(f"include {path}\n\n")
 
     def emptyline(self, amount: int = 1) -> None:
-        with open(self.filename, "a") as f:
-            f.write("\n" * amount)
+        with open(self.filename, "a") as af:
+            af.write("\n" * amount)
 
     def comment(self, content: str, addline: bool = False) -> None:
-        with open(self.filename, "a") as f:
+        with open(self.filename, "a") as af:
             if not addline:
-                f.write(f"# {content}\n")
+                af.write(f"# {content}\n")
             else:
-                f.write(f"# {content}\n\n")
+                af.write(f"# {content}\n\n")
